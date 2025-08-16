193f637de9565668f6c3fb6b2fb83fe460678116
Jed Olvera <jedolvera@gmail.com>
2025-08-16 09:39:52 -0500

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -1,26 +1,24 @@
-<# 
-
-.SYNOPSIS 
+<#
+.SYNOPSIS
     Profile Script. Last Modified 10/06/2025
 
-.DESCRIPTION 
-	something borrowed, something blue, something old
-	
+.DESCRIPTION
+    something borrowed, something blue, something old
+
 .FUNCTION LIST
-	.Get-MachineInfo
-	.ha
-	.Clear-Clipboard
-	.df
-	.get-drivespace
-	.aduser-info
-	.go-365admin
-	.get-loggedonuser
-	.prompt
-	.ioc-search
-	
+    .Get-MachineInfo
+    .ha
+    .Clear-Clipboard
+    .df
+    .get-drivespace
+    .aduser-info
+    .go-365admin
+    .get-loggedonuser
+    .prompt
+    .ioc-search
 
 .FUNCTION Go-365admin
-	Connects to EXO with SSO credentials
+    Connects to EXO with SSO credentials
 
 .FUNCTION Get-LoggedOnUser
     checked logged on user for remote machines. use it to see what examiners i am logged in to.
@@ -28,252 +26,234 @@
     .example 1 Get-LoggedOnUser localhost
     .example 2 Get-LoggedOnUser localhost,remotecomputer
     .example 3 Get-LoggedOnUser (get-content .\computers.txt/csv)
-	
-.FUNCTION prompt
-	marks prompt with admin if logged in as admin
-	uses split path to shorten prompt while showing path in the powershell window title
 
+.FUNCTION prompt
+    marks prompt with admin if logged in as admin
+    uses split path to shorten prompt while showing path in the powershell window title
 #>
 
-$datecon = get-date -UFormat "%A, %b %d %Y %I:%M %p"
+$datecon = Get-Date -UFormat "%A, %b %d %Y %I:%M %p"
 $date = Get-Date -UFormat "%Y-%m-%d"
-$host.ui.RawUI.WindowTitle = " Started $datecon "
-Write-Host "loading, please wait..." -foregroundcolor green
-if( -Not (Test-Path -Path $Env:userprofile\'scripts' ) )
-{
-    New-Item -ItemType directory -Path $Env:userprofile\'scripts'
-	}
-Start-Sleep -s 1
-cls
-write-host $datecon
+$host.UI.RawUI.WindowTitle = " Started $datecon "
+Write-Host "loading, please wait..." -ForegroundColor Green
 
-function Get-MachineInfo($ServerName="localhost")             
-{            
-get-wmiobject win32_computersystem -ComputerName $ServerName |            
-select DNSHostName, Manufacturer, Model, SystemType ,             
-        @{Name="TotalPhysicalMemoryInMB";Expression={"{0:n2}" -f($_.TotalPhysicalMemory/1mb)}},             
-        NumberOfLogicalProcessors, NumberOfProcessors, CurrentTimeZone, DaylightInEffect            
-}# End Get-MachineInfo
+if (-Not (Test-Path -Path "$Env:userprofile\scripts")) {
+    New-Item -ItemType Directory -Path "$Env:userprofile\scripts"
+}
 
+Start-Sleep -Seconds 1
+Clear-Host
+Write-Host $datecon
+
+function Get-MachineInfo {
+    param (
+        $ServerName = "localhost"
+    )
+    Get-WmiObject Win32_ComputerSystem -ComputerName $ServerName |
+        Select-Object DNSHostName, Manufacturer, Model, SystemType,
+            @{Name="TotalPhysicalMemoryInMB";Expression={"{0:n2}" -f ($_.TotalPhysicalMemory/1mb)}},
+            NumberOfLogicalProcessors, NumberOfProcessors, CurrentTimeZone, DaylightInEffect
+}
 
 function ha {
-    Get-History -count $MaximumHistoryCount
+    Get-History -Count $MaximumHistoryCount
 }
 
 function Clear-Clipboard {
-   Add-Type -AssemblyName System.Windows.Forms
-   [System.Windows.Forms.Clipboard]::Clear()
-   Write-Output "Clipboard cleared."
+    Add-Type -AssemblyName System.Windows.Forms
+    [System.Windows.Forms.Clipboard]::Clear()
+    Write-Output "Clipboard cleared."
 }
- 
 Set-Alias -Name cc -Value Clear-Clipboard
 
 function df {
-    $colItems = Get-wmiObject -class "Win32_LogicalDisk" -namespace "root\CIMV2" `
-    -computername localhost
-
+    $colItems = Get-WmiObject -Class "Win32_LogicalDisk" -Namespace "root\CIMV2" -ComputerName localhost
     foreach ($objItem in $colItems) {
-        write $objItem.DeviceID $objItem.Description $objItem.FileSystem `
-            ($objItem.Size / 1GB).ToString("f3") ($objItem.FreeSpace / 1GB).ToString("f3")
-
+        Write-Output "$($objItem.DeviceID) $($objItem.Description) $($objItem.FileSystem) $([math]::Round($objItem.Size / 1GB, 3)) $([math]::Round($objItem.FreeSpace / 1GB, 3))"
     }
 }
 
 function get-drivespace {
-  param( [parameter(mandatory=$true)]$Computer)
-  if ($computer -like "*.com") {$cred = get-credential; $qry = Get-WmiObject Win32_LogicalDisk -filter drivetype=3 -comp $computer -credential $cred }
-  else { $qry = Get-WmiObject Win32_LogicalDisk -filter drivetype=3 -comp $computer }  
-  $qry | select `
-    @{n="drive"; e={$_.deviceID}}, `
-    @{n="GB Free"; e={"{0:N2}" -f ($_.freespace / 1gb)}}, `
-    @{n="TotalGB"; e={"{0:N0}" -f ($_.size / 1gb)}}, `
-    @{n="FreePct"; e={"{0:P0}" -f ($_.FreeSpace / $_.size)}}, `
-    @{n="name"; e={$_.volumeName}} |
-  format-table -autosize
-} #close drivespace
-
-function aduser-info {
-
-param (
-    $who
-)
- get-aduser -server spsc $who -properties * | select-object displayname, `
-                                                Organization, `
-                                                samaccountname, `
-												created, `
-                                                passwordlastset, `
-												whenchanged, `
-												lastlogondate, `
-												passwordneverexpires, `
-												lastbadpasswordattempt, `
-                                                emailaddress, `
-                                                HomeDirectory, `
-                                                state, `
-                                                comment, `
-                                                employeeid, `
-                                                extensionAttribute11, `
-                                                extensionAttribute12, `
-                                                extensionAttribute13, `
-                                                msExchHomeServerName, `
-                                                SID
-}
-
-
-function ioc-search {
-    param ( $badstr,$badpath,$badext )
-
-
-$searchresults = Get-ChildItem $badpath "*.$badext" -Recurse | `
-                 Select-String -Pattern "$badstr" | `
-                 Select-Object -Unique Path
-if ($searchresults -eq $null) { 
-    Write-host "No matches found" -ForegroundColor green
-    } else { 
-    write-host "! possible IOC found in the following files !" -ForegroundColor Red
-    $searchresults | foreach { $_.Path}
-    }
-}
-
-### just need to edit with your account below###
-
-Function Go-365admin {
-Connect-ExchangeOnline -UserPrincipalName 
-}	
-
-function Get-LoggedOnUser {
-#Requires -Version 2.0            
-[CmdletBinding()]            
- Param             
-   (                       
-    [Parameter(Mandatory=$true,
-               Position=0,                          
-               ValueFromPipeline=$true,            
-               ValueFromPipelineByPropertyName=$true)]            
-    [String[]]$ComputerName
-   )#End Param
-
-Begin            
-{            
- Write-Host "`n Checking Users . . . "
- $i = 0            
-}#Begin          
-Process            
-{
-    $ComputerName | Foreach-object {
-    $Computer = $_
-    try
-        {
-            $processinfo = @(Get-WmiObject -class win32_process -ComputerName $Computer -EA "Stop")
-                if ($processinfo)
-                {    
-                    $processinfo | Foreach-Object {$_.GetOwner().User} | 
-                    Where-Object {$_ -ne "NETWORK SERVICE" -and $_ -ne "LOCAL SERVICE" -and $_ -ne "SYSTEM"} |
-                    Sort-Object -Unique |
-                    ForEach-Object { New-Object psobject -Property @{Computer=$Computer;LoggedOn=$_} } | 
-                    Select-Object Computer,LoggedOn
-                }#If
-        }
-    catch
-        {
-            "Cannot find any processes running on $computer" | Out-Host
-        }
-     }#Forech-object(ComputerName)       
-            
-}#Process
-End
-{
-
-}#End
-
-}#Get-LoggedOnUser
-
-function prompt
-{
-
-    # New nice WindowTitle
-    $Host.UI.RawUI.WindowTitle = "PowerShell v" + (get-host).Version.Major + "." + (get-host).Version.Minor + " (" + $pwd.Provider.Name + ") " + $pwd.Path
- 
-    # Admin ?
-    if( (
-        New-Object Security.Principal.WindowsPrincipal (
-            [Security.Principal.WindowsIdentity]::GetCurrent())
-        ).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
-    {
-        # Admin-mark in WindowTitle
-        $Host.UI.RawUI.WindowTitle = "[Admin] " + $Host.UI.RawUI.WindowTitle
- 
-        # Admin-mark on prompt
-		$path = Split-Path -leaf -path (Get-Location)
-        Write-Host "[" -nonewline -foregroundcolor DarkGray
-        Write-Host "Admin" -nonewline -foregroundcolor Red
-        Write-Host "] $path" -nonewline -foregroundcolor DarkGray
-        Write-Host "`:\>" -nonewline -foregroundcolor Cyan	
+    param (
+        [Parameter(Mandatory = $true)]
+        $Computer
+    )
+    if ($Computer -like "*.com") {
+        $cred = Get-Credential
+        $qry = Get-WmiObject Win32_LogicalDisk -Filter "drivetype=3" -ComputerName $Computer -Credential $cred
     }
     else {
-        $path = Split-Path -leaf -path (Get-Location)
-        Write-Host "[" -nonewline -foregroundcolor DarkGray
-        Write-Host "Non-Admin" -nonewline -foregroundcolor Yellow
-        Write-Host "] $path" -nonewline -foregroundcolor DarkGray
-        Write-Host "`:\>" -nonewline -foregroundcolor cyan	
+        $qry = Get-WmiObject Win32_LogicalDisk -Filter "drivetype=3" -ComputerName $Computer
     }
- 
+    $qry | Select-Object `
+        @{n="drive"; e={$_.DeviceID}}, `
+        @{n="GB Free"; e={"{0:N2}" -f ($_.FreeSpace / 1GB)}}, `
+        @{n="TotalGB"; e={"{0:N0}" -f ($_.Size / 1GB)}}, `
+        @{n="FreePct"; e={"{0:P0}" -f ($_.FreeSpace / $_.Size)}}, `
+        @{n="name"; e={$_.VolumeName}} |
+    Format-Table -AutoSize
+}
+
+function aduser-info {
+    param (
+        $who
+    )
+    Get-ADUser -Server spsc $who -Properties * | Select-Object displayname,
+        Organization,
+        samaccountname,
+        created,
+        passwordlastset,
+        whenchanged,
+        lastlogondate,
+        passwordneverexpires,
+        lastbadpasswordattempt,
+        emailaddress,
+        HomeDirectory,
+        state,
+        comment,
+        employeeid,
+        extensionAttribute11,
+        extensionAttribute12,
+        extensionAttribute13,
+        msExchHomeServerName,
+        SID
+}
+
+function ioc-search {
+    param (
+        $badstr,
+        $badpath,
+        $badext
+    )
+
+    $searchresults = Get-ChildItem $badpath "*.$badext" -Recurse |
+        Select-String -Pattern "$badstr" |
+        Select-Object -Unique Path
+
+    if ($null -eq $searchresults) {
+        Write-Host "No matches found" -ForegroundColor Green
+    }
+    else {
+        Write-Host "! possible IOC found in the following files !" -ForegroundColor Red
+        $searchresults | ForEach-Object { $_.Path }
+    }
+}
+
+# just need to edit with your account below
+function Go-365admin {
+    Connect-ExchangeOnline -UserPrincipalName
+}
+
+function Get-LoggedOnUser {
+    #Requires -Version 2.0
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true, Position = 0, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [String[]]$ComputerName
+    )
+    begin {
+        Write-Host "`n Checking Users . . . "
+    }
+    process {
+        $ComputerName | ForEach-Object {
+            $Computer = $_
+            try {
+                $processinfo = @(Get-WmiObject -Class Win32_Process -ComputerName $Computer -ErrorAction Stop)
+                if ($processinfo) {
+                    $processinfo | ForEach-Object { $_.GetOwner().User } |
+                        Where-Object { $_ -ne "NETWORK SERVICE" -and $_ -ne "LOCAL SERVICE" -and $_ -ne "SYSTEM" } |
+                        Sort-Object -Unique |
+                        ForEach-Object { New-Object psobject -Property @{ Computer = $Computer; LoggedOn = $_ } } |
+                        Select-Object Computer, LoggedOn
+                }
+            }
+            catch {
+                "Cannot find any processes running on $Computer" | Out-Host
+            }
+        }
+    }
+    end { }
+}
+
+function prompt {
+    # New nice WindowTitle
+    $Host.UI.RawUI.WindowTitle = "PowerShell v$((Get-Host).Version.Major).$((Get-Host).Version.Minor) ($($pwd.Provider.Name)) $($pwd.Path)"
+
+    # Admin?
+    if (
+        (New-Object Security.Principal.WindowsPrincipal (
+            [Security.Principal.WindowsIdentity]::GetCurrent()
+        )).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+    ) {
+        # Admin-mark in WindowTitle
+        $Host.UI.RawUI.WindowTitle = "[Admin] " + $Host.UI.RawUI.WindowTitle
+
+        # Admin-mark on prompt
+        $path = Split-Path -Leaf -Path (Get-Location)
+        Write-Host "[" -NoNewline -ForegroundColor DarkGray
+        Write-Host "Admin" -NoNewline -ForegroundColor Red
+        Write-Host "] $path" -NoNewline -ForegroundColor DarkGray
+        Write-Host "`:\>" -NoNewline -ForegroundColor Cyan
+    }
+    else {
+        $path = Split-Path -Leaf -Path (Get-Location)
+        Write-Host "[" -NoNewline -ForegroundColor DarkGray
+        Write-Host "Non-Admin" -NoNewline -ForegroundColor Yellow
+        Write-Host "] $path" -NoNewline -ForegroundColor DarkGray
+        Write-Host "`:\>" -NoNewline -ForegroundColor Cyan
+    }
+
     # Show providername if you are outside FileSystem
     if ($pwd.Provider.Name -ne "FileSystem") {
-        Write-Host "[" -nonewline -foregroundcolor DarkGray
-        Write-Host $pwd.Provider.Name -nonewline -foregroundcolor Gray
-        Write-Host "] " -nonewline -foregroundcolor DarkGray
+        Write-Host "[" -NoNewline -ForegroundColor DarkGray
+        Write-Host $pwd.Provider.Name -NoNewline -ForegroundColor Gray
+        Write-Host "] " -NoNewline -ForegroundColor DarkGray
     }
- 
-    # Backspace last \ and write >
-    #Write-Host "`b> test" -nonewline -foregroundcolor Gray
 
     return " "
 }
 
-### old functions i can't toss
+# Old functions I can't toss
 
 <#
 function Get-FolderSizes {
-  [cmdletBinding()]
-  param(
-    [parameter(mandatory=$true)]$Path,
-    [parameter(mandatory=$false)]$SizeMB,
-    [parameter(mandatory=$false)]$ExcludeFolder
-  ) #close param
-  $pathCheck = test-path $path
-  if (!$pathcheck) {"Invalid path. Wants gci's -path parameter."; break}
-  $fso = New-Object -ComObject scripting.filesystemobject
-  $parents = Get-ChildItem $path -Force | where { $_.PSisContainer -and $_.name -ne $ExcludeFolder }
-  $folders = Foreach ($folder in $parents) {
-    $getFolder = $fso.getFolder( $folder.fullname.tostring() )
-    if (!$getFolder.Size) { #for "special folders" like appdata
-      $lengthSum = gci $folder.FullName -recurse -force -ea silentlyContinue | `
-        measure -sum length -ea SilentlyContinue | select -expand sum
-      $sizeMBs = "{0:N0}" -f ($lengthSum /1mb)      
-    } #close if size property is null
-      else { $sizeMBs = "{0:N0}" -f ($getFolder.size /1mb) }
-      #else {$sizeMBs = [int]($getFolder.size /1mb) }
-    New-Object -TypeName psobject -Property @{
-       name = $getFolder.path;
-      sizeMB = $sizeMBs
-    } #close new obj property
-  } #close foreach folder
-  #here's the output
-  $folders | sort @{E={[decimal]$_.sizeMB}} -Descending | ? {[decimal]$_.sizeMB -gt $SizeMB} | ft -auto
-  #calculate the total including contents
-  $sum = $folders | select -expand sizeMB | measure -sum | select -expand sum
-  $sum += ( gci -file $path | measure -property length -sum | select -expand sum ) / 1mb
-  $sumString = "{0:n2}" -f ($sum /1kb)
-  $sumString + " GB total" 
-} #end function
-set-alias gfs Get-FolderSizes
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true)]$Path,
+        [Parameter(Mandatory = $false)]$SizeMB,
+        [Parameter(Mandatory = $false)]$ExcludeFolder
+    )
+    $pathCheck = Test-Path $Path
+    if (!$pathCheck) { "Invalid path. Wants gci's -path parameter."; break }
+    $fso = New-Object -ComObject Scripting.FileSystemObject
+    $parents = Get-ChildItem $Path -Force | Where-Object { $_.PSIsContainer -and $_.Name -ne $ExcludeFolder }
+    $folders = foreach ($folder in $parents) {
+        $getFolder = $fso.GetFolder($folder.FullName.ToString())
+        if (!$getFolder.Size) {
+            $lengthSum = Get-ChildItem $folder.FullName -Recurse -Force -ErrorAction SilentlyContinue |
+                Measure-Object -Sum Length -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Sum
+            $sizeMBs = "{0:N0}" -f ($lengthSum / 1MB)
+        }
+        else {
+            $sizeMBs = "{0:N0}" -f ($getFolder.Size / 1MB)
+        }
+        New-Object -TypeName PSObject -Property @{
+            Name   = $getFolder.Path
+            SizeMB = $sizeMBs
+        }
+    }
+    $folders | Sort-Object @{E = { [decimal]$_.SizeMB }} -Descending | Where-Object { [decimal]$_.SizeMB -gt $SizeMB } | Format-Table -AutoSize
+    $sum = $folders | Select-Object -ExpandProperty SizeMB | Measure-Object -Sum | Select-Object -ExpandProperty Sum
+    $sum += (Get-ChildItem -File $Path | Measure-Object -Property Length -Sum | Select-Object -ExpandProperty Sum) / 1MB
+    $sumString = "{0:n2}" -f ($sum / 1KB)
+    $sumString + " GB total"
+}
+Set-Alias gfs Get-FolderSizes
 #>
 
 <#
 function title-rename {
-	param( $newtitle
-	 )
-	$host.ui.RawUI.WindowTitle = " [ $newtitle ] "
-	}
+    param($newtitle)
+    $host.UI.RawUI.WindowTitle = " [ $newtitle ] "
+}
 #>
-
